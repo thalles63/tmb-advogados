f0115c1c7d26292149d58ccf4d07e7fa882ff764
thalles <thalles63@gmail.com>
2021-05-04 23:01:07 -0300

    mudanças em imagens e lazy load nelas

--- js/scripts.js
+++ js/scripts.js
@@ -46,3 +46,15 @@ function fadeoutbg() {
       bg.style.zIndex = -1;
    }, 300);
 }
+let observer = new IntersectionObserver((entries, observer) => {
+   entries.forEach(entry => {
+      if (!entry.isIntersecting) return;
+      const image = entry.target;
+      image.src = image.src.replace('w_10/', '');
+      observer.unobserve(image);
+   })
+}, {})
+const images = document.querySelectorAll("img");
+images.forEach(image => {
+   observer.observe(image);
+})
